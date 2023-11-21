9d68512ff3b77c32b8d695232ff6a29d6761f02d
TommyRabes <rabesalama.tommy@gmail.com>
2023-11-21 23:07:47 +0300

    Kick off home page first section

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../components/button/button.component';
+
+const COMPONENTS = [ButtonComponent];
 
 @Component({
   selector: 'maw-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ...COMPONENTS],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
