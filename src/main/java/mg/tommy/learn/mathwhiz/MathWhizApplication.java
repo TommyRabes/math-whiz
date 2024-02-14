ec0966704d3d85ab6697e4ce6f4e7d10bf63a330
TommyRabes <rabesalama.tommy@gmail.com>
2024-02-14 23:50:59 +0300

    Setup Spring Boot backend server

---
+++ src/main/java/mg/tommy/learn/mathwhiz/MathWhizApplication.java
@@ -0,0 +1,13 @@
+package mg.tommy.learn.mathwhiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathWhizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathWhizApplication.class, args);
+	}
+
+}
